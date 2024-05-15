5e4135aa4a25da4ce5c41fd3ace3bf2d8679c355
Kam <erkamyaman35@gmail.com>
2024-05-15 16:52:43 +0300

    mongooese

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors'); // Import the cors package
 const app = express();
+const mongoose = require('mongoose');
 const port = 3000;
 
 // Enable CORS for all routes
@@ -12,6 +13,14 @@ const data = [
     { id: 2, name: 'Item 2', description: 'This is item 2' },
     { id: 3, name: 'Item 3', description: 'This is item 3' },
 ];
+
+mongoose.connect('mongodb+srv://erkamyaman35:Tatil.18@backenddb.hs5fa8a.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB')
+    .then(() => {
+        console.log('success')
+    }).catch(() => {
+        console.log('fail')
+    })
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
