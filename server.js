49fec280b02eb03a659e3ed284d81dc30b28a2e1
Kam <erkamyaman35@gmail.com>
2024-05-17 16:48:21 +0300

    vercel

--- server.js
+++ server.js
@@ -4,16 +4,10 @@ const app = express();
 const mongoose = require('mongoose');
 const port = 3000;
 
-// Enable CORS for all routes
 app.use(cors());
 app.use(express.json())
 
-// Sample data
-const data = [
-    { id: 1, name: 'Item 1', description: 'This is item 1' },
-    { id: 2, name: 'Item 2', description: 'This is item 2' },
-    { id: 3, name: 'Item 3', description: 'This is item 3' },
-];
+
 
 mongoose.connect('mongodb+srv://erkamyaman35:Tatil.18@backenddb.hs5fa8a.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB')
     .then(() => {
