de717bc3aa29ed5a809196c7f776fa77b468a3fd
Kam <88717125+yamanerkam@users.noreply.github.com>
2024-09-08 22:38:33 +0300

    Update Mongo.js
    delete

--- Mongo.js
+++ Mongo.js
@@ -1,5 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://erkamyaman35:Tatil.18@backenddb.hs5fa8a.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB";
+const uri = "";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
